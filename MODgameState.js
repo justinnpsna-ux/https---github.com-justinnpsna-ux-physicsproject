02dd9ee5c7b46a2b7368dcdbe9aa181b3977d7f5
Justin Prasetyo <justinnp.sna@gmail.com>
2026-06-30 22:10:54 -0400

    commit

---
+++ MODgameState.js
@@ -0,0 +1,26 @@
+//module for game state class
+import { canvas, ctx, nextCircleId, cellSize, 
+    totalColumns, grid, cellChecks, isPlayerCreated, 
+    interacted } from './index.js';
+
+//arrays
+import { player, faller, enemies, bullets, badBullets, swinger } from './index.js'
+
+export class GameState { 
+    constructor() {
+
+    }
+
+    drawMainMenu() {
+
+    }
+
+    drawDeathMenu() {
+
+    }
+
+    resetGame() {
+        
+    }
+    
+}
